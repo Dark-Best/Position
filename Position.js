de0dd93b134c255d91602a6cfa9258c6de5d68fd
seb03000 <avatardu33@gmail.com>
2017-11-15 18:14:20 +0100

    Add files via upload

---
+++ Position.js
@@ -0,0 +1,22 @@
+﻿exports.action = function(data, callback, config, SARAH){
+
+console.log('[connexion en cours....] ','Lancement');
+// CONFIG
+config = config.modules.Position;
+if (!config.clef){
+console.log("Mobbyway config missing");
+callback({ 'tts': "J'ai besoin de ton identifiant Mobbyway. Renseigne le dans le fichier de propriété" });
+return;
+}
+var token = config.clef;
+var url = 'http://www.mobbyway.com/Process/MobbyBySarah.php?token='+token; 
+var request = require('request');
+request({ 'uri' : url }, function (err, response, body){
+if (err || response.statusCode != 200) {
+callback({'tts': "Je n'arrive pas à me connecter à Mobbyway"});
+return;
+}
+//callback({ 'tts': "je n'ai pas compris" });
+callback({ 'tts': body}); 
+  });
+}
